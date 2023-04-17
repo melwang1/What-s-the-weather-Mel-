29055eb52615e7b660463524c483b97ef1261f21
Melanie Wang <melaniiewang@hotmail.com>
2023-04-16 21:07:57 -0400

    created global variables that were being used locally, create function for getweatherforecast to fetch API and store in local storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,26 @@
 var search = document.getElementById ("getweather");
+var apiKey = "350b763106d9eec4d6de49b9cfcf9a17";
+var cityName;
+var apiUrl;
 
 search.addEventListener("click",function(event){
     event.preventDefault()
-    var city = document.getElementById ("examplecityname").value
-    console.log(city)
+    cityName = document.getElementById ("examplecityname").value
+    apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+    console.log(cityName)
+    console.log(apiUrl)
+    getweatherforecast();
 })
+
+
+function getweatherforecast () {
+
+fetch (apiUrl)
+    .then(function(response){
+    return response.json();
+})
+    .then(function(data){
+        console.log (data)
+    localStorage.setItem("cityName", JSON.stringify(data));
+    })
+}    
